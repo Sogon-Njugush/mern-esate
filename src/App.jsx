b5cafc54b76771885176996ea1d6cffcfb99e8fc
Sogon-Njugush <josephnjuguna00@gmail.com>
2023-10-10 17:42:38 +0300

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+// import React from 'react';
+import{Route, Routes, BrowserRouter} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <div>
-      <h3 className='text-red-700'>Welcome to react vite programming</h3>
-    </div>
+    <BrowserRouter>
+      <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/sign-in' element={<SignIn/>}/>
+       <Route path='/sign-up' element={<SignUp/>}/>
+       <Route path='/profile' element={<Profile/>}/>
+       <Route path='/about' element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
